d78f1bd8b990b80c93bb1c1427eee0b9f99f6a05
Manohar Reddy <b.manu199@gmail.com>
2023-12-19 18:01:38 +0100

    add EKS infra

--- main.tf
+++ main.tf
@@ -19,8 +19,21 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs                     = ["us-east-2a", "us-east-2b", ]
+  private_subnets          = ["10.0.1.0/24", "10.0.3.0/24"]
   public_subnets          = ["10.0.2.0/24", "10.0.4.0/24"]
   map_public_ip_on_launch = true
+
+  enable_nat_gateway      = false
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb"                    = 1
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb"           = 1
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
 
   tags = {
     Terraform   = "true"
@@ -186,6 +199,67 @@ sudo  yum install -y pip
 pip install sbcli
 sbcli caching-node deploy
 EOF
+}
+
+
+
+################################################################################
+# EKS
+################################################################################
+module "eks" {
+  count   = 1
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.16.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.28"
+
+  cluster_endpoint_private_access = true # default is true
+  cluster_endpoint_public_access  = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  enable_irsa = true
+
+  eks_managed_node_group_defaults = {
+    disk_size                  = 30
+    iam_role_attach_cni_policy = true
+  }
+
+  eks_managed_node_groups = {
+    eks-nodes = {
+      desired_size = 3
+      min_size     = 2
+      max_size     = 4
+
+      labels = {
+        role = "general"
+      }
+
+      instance_types                       = ["t3.medium"]
+      capacity_type                        = "ON_DEMAND"
+      key_name                             = "simplyblock-us-east-2.pem"
+      worker_additional_security_group_ids = [aws_security_group.container_inst_sg.id]
+    }
+  }
+
+  tags = {
+    Name        = "simplyblock-eks-cluster"
+    Environment = "dev"
+  }
 }
 
 
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "cluster_name" {
+  default     = "simplyblock-eks-cluster"
+  description = "EKS Cluster name"
+}
+
