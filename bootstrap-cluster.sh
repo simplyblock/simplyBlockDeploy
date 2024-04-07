fc0ec79f085bc2075c2b3fea9a8105346731e942
Manohar Reddy <b.manu199@gmail.com>
2024-04-07 12:58:54 +0200

    use local state (#17)

--- bootstrap-cluster.sh
+++ bootstrap-cluster.sh
@@ -6,8 +6,10 @@ SECRET_VALUE=$(terraform output -raw secret_value)
 KEY_NAME=$(terraform output -raw key_name)
 
 if [[ -n "$SECRET_VALUE" ]]; then
+    rm $HOME/.ssh/$KEY_NAME
     echo $SECRET_VALUE > $HOME/.ssh/$KEY_NAME
     KEY=$HOME/.ssh/$KEY_NAME
+    chmod 400 $KEY
 else
     echo "Failed to retrieve secret value. Falling back to default key."
 fi
