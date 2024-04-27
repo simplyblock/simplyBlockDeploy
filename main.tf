742d5a619281c586748a5670409a9c61a41b848e
Manohar Reddy <b.manu199@gmail.com>
2024-04-27 12:45:14 +0200

    update outputs to display instance name, type, public/private IP (#31)

--- main.tf
+++ main.tf
@@ -222,8 +222,8 @@ EOF
 }
 
 resource "aws_instance" "storage_nodes" {
-  for_each               = local.snodes
-  
+  for_each = local.snodes
+
   ami                    = local.region_ami_map[var.region] # RHEL 9
   instance_type          = var.storage_nodes_instance_type
   key_name               = local.selected_key_name
@@ -257,14 +257,14 @@ resource "aws_ebs_volume" "storage_nodes_ebs" {
 }
 
 resource "aws_ebs_volume" "storage_nodes_ebs2" {
-  for_each          = local.node_disks
+  for_each = local.node_disks
 
   availability_zone = data.aws_availability_zones.available.names[1]
   size              = var.storage_nodes_ebs_size2
 }
 
 resource "aws_volume_attachment" "attach_sn2" {
-  for_each    = local.node_disks
+  for_each = local.node_disks
 
   device_name = each.value.disk_dev_path
   volume_id   = aws_ebs_volume.storage_nodes_ebs2[each.key].id
@@ -398,28 +398,12 @@ module "eks" {
   }
 }
 
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-
 output "storage_private_ips" {
   value = join(" ", [for inst in aws_instance.storage_nodes : inst.private_ip])
 }
 
-output "storage_public_ips" {
-  value = join(" ", [for inst in aws_instance.storage_nodes : inst.public_ip])
-}
-
-output "mgmt_private_ips" {
-  value = aws_instance.mgmt_nodes[*].private_ip
-}
-
 output "mgmt_public_ips" {
   value = join(" ", aws_instance.mgmt_nodes[*].public_ip)
-}
-
-output "extra_nodes_private_ips" {
-  value = aws_instance.extra_nodes[*].private_ip
 }
 
 output "extra_nodes_public_ips" {
@@ -429,7 +413,30 @@ output "extra_nodes_public_ips" {
 output "key_name" {
   value = local.selected_key_name
 }
+
 output "secret_value" {
   sensitive = true
   value     = data.aws_secretsmanager_secret_version.simply.secret_string
+}
+
+output "mgmt_node_details" {
+  value = { for i, instance in aws_instance.mgmt_nodes :
+    instance.tags["Name"] => {
+      type       = instance.instance_type
+      public_ip  = instance.public_ip
+      private_ip = instance.private_ip
+    }
+  }
+  description = "Details of the mgmt nodes."
+}
+
+output "storage_node_details" {
+  value = { for i, instance in aws_instance.storage_nodes :
+    instance.tags["Name"] => {
+      type       = instance.instance_type
+      public_ip  = instance.public_ip
+      private_ip = instance.private_ip
+    }
+  }
+  description = "Details of the storage node nodes."
 }
