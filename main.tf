7f806c5bd87df271a1be970170561c6c581cd7fd
Manohar Reddy <b.manu199@gmail.com>
2024-04-30 21:57:39 +0200

    revert security groups

--- main.tf
+++ main.tf
@@ -97,27 +97,27 @@ resource "aws_security_group" "container_inst_sg" {
     protocol  = -1
     self      = true
   }
-  # ingress {
-  #   from_port   = 80
-  #   to_port     = 80
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  #   description = ""
-  # }
-  # ingress {
-  #   from_port   = 443
-  #   to_port     = 443
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  #   description = ""
-  # }
-  # ingress {
-  #   from_port   = 2222
-  #   to_port     = 2222
-  #   protocol    = "tcp"
-  #   cidr_blocks = var.whitelist_ips
-  #   description = ""
-  # }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = ""
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = ""
+  }
+  ingress {
+    from_port   = 2222
+    to_port     = 2222
+    protocol    = "tcp"
+    cidr_blocks = var.whitelist_ips
+    description = ""
+  }
 
 
   ## Egress traffic
