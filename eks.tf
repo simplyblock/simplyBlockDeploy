90fc2e4155bc27b988cbfd47c59c140e8b10a767
geoffrey1330 <israelgeoffrey13@gmail.com>
2024-05-29 17:00:37 +0100

    added eks security group

--- eks.tf
+++ eks.tf
@@ -14,6 +14,14 @@ resource "aws_security_group" "eks_nodes_sg" {
   }
 
   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.whitelist_ips
+    description = ""
+  }
+  
+  ingress {
     from_port       = 8080
     to_port         = 8080
     protocol        = "tcp"
@@ -45,6 +53,14 @@ resource "aws_security_group" "eks_nodes_sg" {
     description = "caching node"
   }
   
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "allow traffic from API gateway to Mgmt nodes"
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
