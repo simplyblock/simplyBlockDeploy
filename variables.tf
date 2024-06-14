dcbc7cfbb1c671906be051fd8fd76284fe2e7600
hamdy-simplyblock <159147829+hamdy-simplyblock@users.noreply.github.com>
2024-06-14 21:16:29 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -24,8 +24,8 @@ variable "sbcli_cmd" {
   description = "sbcli command to be used"
   type        = string
   validation {
-    condition     = can(regex("^sbcli-dev$|^sbcli-release$|^sbcli-mig$|^sbcli-mc$", var.sbcli_cmd))
-    error_message = "Invalid sbcli command. Please choose one of: sbcli-dev, sbcli-release, sbcli-mig, sbcli-mc."
+    condition     = can(regex("^sbcli-dev$|^sbcli-release$|^sbcli-mig$|^sbcli-mc$|^sbcli-pre$", var.sbcli_cmd))
+    error_message = "Invalid sbcli command. Please choose one of: sbcli-dev, sbcli-release, sbcli-mig, sbcli-mc, sbcli-pre."
   }
 }
 
