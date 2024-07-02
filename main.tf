e4b55e44b9b19dffc72800a637ed183192bbf3d1
hamdy-simplyblock <159147829+hamdy-simplyblock@users.noreply.github.com>
2024-07-02 18:19:46 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -662,7 +662,7 @@ resource "aws_volume_attachment" "attach_sn" {
 # can be used for testing caching nodes
 resource "aws_instance" "extra_nodes" {
   count                  = var.extra_nodes
-  ami                    = local.region_ami_map[var.region] # RHEL 9
+  ami                    = "ami-08f9f3bb075432791"
   instance_type          = var.extra_nodes_instance_type
   key_name               = local.selected_key_name
   vpc_security_group_ids = [aws_security_group.extra_nodes_sg.id]
