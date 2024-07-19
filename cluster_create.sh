2f6c3ba1ebf646fb48832b8ecd5e23a7ef0e24e3
Manohar Reddy <b.manu199@gmail.com>
2024-07-19 19:48:43 +0530

    add create_cluster.sh file

---
+++ cluster_create.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+namespace="simplyblock"
+sbcli_pkg="sbcli-pre"
+spdk_image="simplyblock/spdk:faster-bdev-startup-latest"
+#terraform destroy --auto-approve
+
+# review the resources
+terraform init
+
+### switch to workspace
+terraform workspace select -or-create "$namespace"
+
+terraform plan
+
+# create initial infra
+terraform apply -var mgmt_nodes=0 -var storage_nodes=0 -var extra_nodes=0 \
+                 -var mgmt_nodes_instance_type="m6i.xlarge" -var storage_nodes_instance_type="i3en.6xlarge" \
+                 -var sbcli_cmd="sbcli-pre" -var extra_nodes_instance_type=m6gd.xlarge \
+                 -var volumes_per_storage_nodes=0 -var region=eu-central-1 -var extra_nodes_arch=arm64
+
+
+# Specifying the instance types to use
+terraform apply -var mgmt_nodes=1 -var storage_nodes=3 -var extra_nodes=0 \
+                 -var mgmt_nodes_instance_type="m6i.xlarge" -var storage_nodes_instance_type="i3en.6xlarge" \
+                 -var sbcli_cmd="sbcli-pre" -var extra_nodes_instance_type=m6gd.xlarge \
+                 -var volumes_per_storage_nodes=0 -var region=eu-central-1 -var extra_nodes_arch=arm64
+
+# Save terraform output to a file
+terraform output -json > outputs.json
+
+# The boostrap-cluster.sh creates the KEY in `.ssh` directory in the home directory
+
+chmod +x ./bootstrap-cluster.sh
+# specifying cluster argument to use
+# --iobuf_small_pool_count 10000 --iobuf_large_pool_count 25000 \
+# --iobuf_large_pool_count 16384 --iobuf_small_pool_count 131072
+./bootstrap-cluster.sh --memory 16g  --cpu-mask 0x3 \
+                       --sbcli-cmd "$sbcli_pkg" --spdk-image "$spdk_image" \
+                       --iobuf_large_pool_count 16384 --iobuf_small_pool_count 131072 \
+                       --log-del-interval 300m --metrics-retention-period 2h
