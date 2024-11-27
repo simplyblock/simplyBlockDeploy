103753dcffe1bac2fc0089653e5076addb2eeaaa
Alexander Sheredin <alexander@simplyblock.io>
2024-11-28 01:57:01 +0300

    changed storage node instance type

--- create_cluster.sh
+++ create_cluster.sh
@@ -17,9 +17,11 @@ terraform init -reconfigure \
 ### switch to workspace
 terraform workspace select -or-create "$namespace"
 
+# terraform apply -var mgmt_nodes=1 -var storage_nodes=0 -var extra_nodes=0 --auto-approve
+
 # Specifying the instance types to use
 terraform apply -var mgmt_nodes=1 -var storage_nodes=4 -var extra_nodes=0 -var "storage_nodes_arch=arm64" \
-                -var mgmt_nodes_instance_type="m6i.xlarge" -var storage_nodes_instance_type="im4gn.4xlarge" \
+                -var mgmt_nodes_instance_type="m6i.xlarge" -var storage_nodes_instance_type="c6gd.2xlarge" \
                 -var extra_nodes_instance_type="m6i.large" -var sbcli_cmd="$sbcli_cmd" \
                 -var volumes_per_storage_nodes=0 --auto-approve
 
@@ -33,7 +35,7 @@ chmod +x ./bootstrap-cluster.sh
 ./bootstrap-cluster.sh --sbcli-cmd "$sbcli_cmd" --disable-ha-jm \
                        --distr-ndcs 2 --distr-npcs 1 --cap-crit 99 --cap-warn 94 --prov-cap-crit 500  \
                        --prov-cap-warn 200 --distr-bs 4096 --distr-chunk-bs 4096 \
-                       --spdk-debug --max-lvol 200 --max-snap 200 --max-prov 30T --number-of-devices 1 \
+                       --spdk-debug --max-lvol 200 --max-snap 200 --max-prov 10T --number-of-devices 1 \
                        --partitions 1 --log-del-interval 300m --metrics-retention-period 2h \
                        --number-of-distribs 5 \
                        --contact-point $CONTACT_POINT
