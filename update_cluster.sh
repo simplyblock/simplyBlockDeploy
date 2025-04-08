75b8e02882c2af2809b058fe56d7fb71ec8e9d85
Manohar Reddy <manohar@simplyblock.io>
2025-04-08 20:58:45 +0200

    Update update_cluster.sh: Use ECR Image

--- update_cluster.sh
+++ update_cluster.sh
@@ -26,12 +26,12 @@ while [[ $# -gt 0 ]]; do
 done
 
 get_service_ids() {
-    docker service ls | grep simplyblock/simplyblock | awk '{print $1}'
+    docker service ls | grep public.ecr.aws/simply-block/simplyblock | awk '{print $1}'
 }
 
 pip install ${SBCLI_CMD} --upgrade
 docker image pull simplyblock/simplyblock:${IMAGE_TAG}
 service_ids=$(get_service_ids)
 for service_id in ${service_ids}; do
-    docker service update "$service_id" --image simplyblock/simplyblock:${IMAGE_TAG} --force
+    docker service update "$service_id" --image public.ecr.aws/simply-block/simplyblock:${IMAGE_TAG} --force
 done
