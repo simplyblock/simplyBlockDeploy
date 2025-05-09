715a86e15d4b75633840d97e9345e3293117e420
Raunak Jalan <ronakjalan98@gmail.com>
2025-05-09 13:21:23 +0530

    Adding check for k8s node before running deploy

--- bootstrap-cluster.sh
+++ bootstrap-cluster.sh
@@ -281,7 +281,11 @@ install_sbcli_on_node() {
         ssh_exec "$node_ip" "
             echo ${configure_cmd} > /root/sn_deploy.log 2>&1
             $configure_cmd >> /root/sn_deploy.log 2>&1
-            ${SBCLI_CMD} sn deploy --ifname eth0 >> /root/sn_deploy.log 2>&1 &
+            if [ \"$K8S_SNODE\" == \"true\" ]; then
+                :
+            else
+                ${SBCLI_CMD} sn deploy --ifname eth0 >> /root/sn_deploy.log 2>&1 &
+            fi
         "
     fi
 }
