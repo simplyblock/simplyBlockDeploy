5e5e255f5a8f7b3486d742a94021b2061c8f15a5
Max Schettler <max.schettler@posteo.de>
2025-05-14 15:32:30 +0200

    Use idiomatic way of GitHub output

--- bare-metal/bootstrap-k3s.sh
+++ bare-metal/bootstrap-k3s.sh
@@ -39,8 +39,8 @@ IFS=' ' read -ra mnodes <<<"$mnodes"
 storage_private_ips=$STORAGE_PRIVATE_IPS
 sec_storage_private_ips=$SEC_STORAGE_PRIVATE_IPS
 
-echo "::set-output name=KEY::$KEY"
-echo "::set-output name=extra_node_ip::${mnodes[0]}"
+echo "KEY=$KEY" >> $GITHUB_OUTPUT
+echo "extra_node_ip=${mnodes[0]}" >> $GITHUB_OUTPUT
 
 
 echo "cleaning up old K8s cluster..."
